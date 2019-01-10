502f806a4313b3db2499f21f2939c0ecdea67494
spdskatr <25078279+spdskatr@users.noreply.github.com>
2019-01-10 11:23:24 +1100

    add post method

--- owoify.py
+++ owoify.py
@@ -6,12 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, world!"
 
-@app.route("/owoify")
+@app.route("/owoify", methods=['GET', 'POST'])
 def owoify():
 	text = request.values.get("text", "owo")
 	return text.replace("l", "w").replace("r", "w").replace("L", "W").replace("R", "W")
 
 if __name__ == "__main__":
     app.run()
-
 
