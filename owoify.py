f1842007fb6982a8429283a082520478a54d455a
spdskatr <25078279+spdskatr@users.noreply.github.com>
2019-01-10 11:09:18 +1100

    Add owoify

---
+++ owoify.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, world!"
+
+@app.route("/owoify")
+def owoify():
+	text = request.values.get("text", "owo")
+	return text.replace("l", "w").replace("r", "w").replace("L", "W").replace("R", "W")
+
+if __name__ == "__main__":
+    app.run()
+
+
